4bdaf25683f4b0300d76df342571aa96d2305032
Jamie Magee <jamie.magee@gmail.com>
2017-03-23 23:59:30 +0100

    Fix pep8 issues

--- tests/test_const.py
+++ tests/test_const.py
@@ -5,8 +5,9 @@ from pyrow import const
 
 
 class ConstTests(TestCase):
-    SEM_VER = re.compile('^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.' \
-                         '(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$')
+    SEM_VER = re.compile('^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-'
+                         'zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.['
+                         '0-9a-zA-Z-]+)*)?$')
 
     def test_semantic_version(self):
         self.assertTrue(
