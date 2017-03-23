7bc307d5edeaac86073ff275b78b74b204f05037
Jamie Magee <jamie.magee@gmail.com>
2017-03-23 23:56:46 +0100

    Add test to check version

---
+++ tests/test_const.py
@@ -0,0 +1,14 @@
+import re
+from unittest import TestCase
+
+from pyrow import const
+
+
+class ConstTests(TestCase):
+    SEM_VER = re.compile('^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.' \
+                         '(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$')
+
+    def test_semantic_version(self):
+        self.assertTrue(
+            self.SEM_VER.fullmatch(const.__version__)
+        )
